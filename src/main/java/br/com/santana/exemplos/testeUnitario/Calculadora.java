8dc88432b520469aa1af43ff98a9815bd3c19cfa
daniloSantana <danilostna@gmail.com>
2021-04-13 22:34:32 -0300

    Utilizando Mocks

--- src/main/java/br/com/santana/exemplos/testeUnitario/Calculadora.java
+++ src/main/java/br/com/santana/exemplos/testeUnitario/Calculadora.java
@@ -1,7 +1,7 @@
 package br.com.santana.exemplos.testeUnitario;
 
 public class Calculadora {
-    public static int soma(int x, int y){
+    public int soma(int x, int y){
         return x + y;
     }
 }
--- src/test/java/br/com/santana/exemplos/testeUnitario/CalculadoraTest.java
+++ src/test/java/br/com/santana/exemplos/testeUnitario/CalculadoraTest.java
@@ -2,13 +2,25 @@ package br.com.santana.exemplos.testeUnitario;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-class CalculadoraTest {
-
+public class CalculadoraTest {
     @Test
-    void soma() {
+    public void testSoma() {
         Calculadora calc = new Calculadora();
         assertEquals(5, calc.soma(2, 3));
     }
+
+    @Test
+    public void testSomaMockito(){
+        Calculadora calc = mock(Calculadora.class);
+        when(calc.soma(3, 3)).thenReturn(10);
+
+        assertEquals(10, calc.soma(3, 3));
+    }
+
+
 }
