b1f9d02787620325553db22717aa66ef5aef8732
Danilo Santana <danilostna@gmail.com>
2023-09-16 00:19:17 -0300

    Desafio palavras duplicadas, solucao com regex

---
+++ src/main/java/br/com/santana/desafios/PalavrasDuplicadas.java
@@ -0,0 +1,76 @@
+package br.com.santana.desafios;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PalavrasDuplicadas {
+
+    public static void main(String[] args) {
+
+        String regex = "\\b(\\w+)(?:\\W+\\1\\b)+";
+        Pattern p = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+
+        List<String> casoDeTeste = buildCasoDeTeste();
+        List<String> resultado = new ArrayList<>();
+        int index = 0;
+
+        while (index < casoDeTeste.size()) {
+            String input = casoDeTeste.get(index);
+
+            Matcher m = p.matcher(input);
+
+            // Check for subsequences of input that match the compiled pattern
+            while (m.find()) {
+                input = input.replaceAll(m.group(), m.group(1));
+            }
+
+            // Prints the modified sentence.
+            System.out.println(input);
+            resultado.add(input);
+            index++;
+        }
+
+        checkResultado(resultado);
+    }
+
+    private static void checkResultado(List<String> resultado) {
+        List<String> expected = new ArrayList<>();
+        expected.add("Goodbye bye world");
+        expected.add("Sam went to his business");
+        expected.add("Reya is the best player in eye game");
+        expected.add("in inthe");
+        expected.add("Hello Ab");
+
+        boolean sucesso = true;
+        int index = 0;
+
+        if(resultado.size() != expected.size()) {
+            System.out.println("Você falhou miseravelmente");
+        }
+
+        while (index < resultado.size()) {
+            sucesso = resultado.get(index).equals(expected.get(index));
+            index++;
+        }
+
+        if (sucesso) {
+            System.out.println("Parabéns, você está próximo de se tornar um code master!");
+        } else {
+            System.out.println("Quase lá, estude mais!");
+        }
+    }
+
+    private static List<String> buildCasoDeTeste() {
+        ArrayList<String> exemplos = new ArrayList<>();
+        exemplos.add("Goodbye bye bye world world world");
+        exemplos.add("Sam went went to to to his business");
+        exemplos.add("Reya is is the the best player in eye eye game");
+        exemplos.add("in inthe");
+        exemplos.add("Hello hello Ab aB");
+
+        return exemplos;
+    }
+
+}
